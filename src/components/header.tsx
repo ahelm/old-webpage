b401f148975fdaafd8c6320900727babb60ea74d
Anton Helm <anton.helm@tecnico.ulisboa.pt>
2020-12-07 15:48:25 +0000

    formats header.tsx

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,37 +1,32 @@
-import React, { CSSProperties } from 'react';
-import { Link } from 'gatsby';
-import {
-  FaTwitter, FaLinkedin, FaGithub, FaRegEnvelope,
-} from 'react-icons/fa';
-import PageLogo from './logo';
+import { Link } from "gatsby";
+import React, { CSSProperties } from "react";
+import { FaGithub, FaLinkedin, FaRegEnvelope, FaTwitter } from "react-icons/fa";
+import PageLogo from "./logo";
 
 const linkStyle: CSSProperties = {
-  textDecoration: 'none',
-  margin: '0 0.8rem',
+  textDecoration: "none",
+  margin: "0 0.8rem",
 };
 
 const activeStyle: CSSProperties = {
-  textDecoration: 'underline',
+  textDecoration: "underline",
 };
 
 const Header = () => (
-  <header
-    style={{
-    }}
-  >
+  <header style={{}}>
     <div
       style={{
-        margin: '0 auto',
+        margin: "0 auto",
         maxWidth: 760,
-        padding: '1.4rem 1.0875rem 1rem',
-        display: 'flex',
-        justifyContent: 'space-evenly',
-        flexWrap: 'wrap',
+        padding: "1.4rem 1.0875rem 1rem",
+        display: "flex",
+        justifyContent: "space-evenly",
+        flexWrap: "wrap",
       }}
     >
       <div
         style={{
-          flex: '1',
+          flex: "1",
         }}
       >
         <Link to="/">
@@ -40,40 +35,49 @@ const Header = () => (
       </div>
       <div
         style={{
-          flex: '2.3',
-          display: 'flex',
-          justifyContent: 'center',
-          alignItems: 'center',
+          flex: "2.3",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
         }}
       >
         <div>
           <h1
             style={{
-              marginBottom: '1rem',
-              textAlign: 'center',
+              marginBottom: "1rem",
+              textAlign: "center",
             }}
           >
-            <Link to="/" style={linkStyle} activeStyle={activeStyle}>About</Link>
-            <Link to="/latest" style={linkStyle} activeStyle={activeStyle}>Latest</Link>
-            <Link to="/cv" style={linkStyle} activeStyle={activeStyle}>CV</Link>
+            <Link to="/" style={linkStyle} activeStyle={activeStyle}>
+              About
+            </Link>
+            <Link to="/latest" style={linkStyle} activeStyle={activeStyle}>
+              Latest
+            </Link>
+            <Link to="/cv" style={linkStyle} activeStyle={activeStyle}>
+              CV
+            </Link>
           </h1>
           <div
             style={{
-              textAlign: 'center',
-              fontSize: '2em',
+              textAlign: "center",
+              fontSize: "2em",
             }}
           >
             <a href="https://twitter.com/example" aria-label="Twitter">
-              <FaTwitter style={{ margin: '10' }} />
+              <FaTwitter style={{ margin: "10" }} />
             </a>
             <a href="https://github.com/example" aria-label="Github">
-              <FaGithub style={{ margin: '10' }} />
+              <FaGithub style={{ margin: "10" }} />
             </a>
-            <a href="https://www.linkedin.com/in/example/" aria-label="Linkedin">
-              <FaLinkedin style={{ margin: '10' }} />
+            <a
+              href="https://www.linkedin.com/in/example/"
+              aria-label="Linkedin"
+            >
+              <FaLinkedin style={{ margin: "10" }} />
             </a>
             <a href="mailto:a.helm87@gmail.com" aria-label="Mail">
-              <FaRegEnvelope style={{ margin: '10' }} />
+              <FaRegEnvelope style={{ margin: "10" }} />
             </a>
           </div>
         </div>
