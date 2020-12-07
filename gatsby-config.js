129fa6380e3df9356d900593bbe923567b646f48
Anton Helm <anton.helm@tecnico.ulisboa.pt>
2020-12-07 15:47:51 +0000

    formats gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,49 +1,49 @@
 module.exports = {
   siteMetadata: {
-    title: 'ahelm.github.io',
-    description: 'Personal webpage of Anton Helm',
-    author: 'Anton Helm',
+    title: "ahelm.github.io",
+    description: "Personal webpage of Anton Helm",
+    author: "Anton Helm",
   },
   plugins: [
-    'gatsby-plugin-react-helmet',
+    "gatsby-plugin-react-helmet",
     {
-      resolve: 'gatsby-source-filesystem',
+      resolve: "gatsby-source-filesystem",
       options: {
-        name: 'images',
+        name: "images",
         path: `${__dirname}/src/images`,
       },
     },
-    'gatsby-transformer-sharp',
-    'gatsby-plugin-sharp',
+    "gatsby-transformer-sharp",
+    "gatsby-plugin-sharp",
     {
-      resolve: 'gatsby-plugin-manifest',
+      resolve: "gatsby-plugin-manifest",
       options: {
-        name: 'ahelm.github.io',
-        short_name: 'ahelm.github.io',
-        start_url: '/',
-        background_color: '#202124',
-        theme_color: '#202124',
-        display: 'browser',
-        icon: 'src/images/ahelm-icon.svg', // This path is relative to the root of the site.
+        name: "ahelm.github.io",
+        short_name: "ahelm.github.io",
+        start_url: "/",
+        background_color: "#202124",
+        theme_color: "#202124",
+        display: "browser",
+        icon: "src/images/ahelm-icon.svg", // This path is relative to the root of the site.
       },
     },
-    'gatsby-plugin-typescript',
-    'gatsby-plugin-twitter',
+    "gatsby-plugin-typescript",
+    "gatsby-plugin-twitter",
     {
-      resolve: 'gatsby-source-filesystem',
+      resolve: "gatsby-source-filesystem",
       options: {
         path: `${__dirname}/src/posts`,
-        name: 'posts',
+        name: "posts",
       },
     },
-    'gatsby-transformer-remark',
+    "gatsby-transformer-remark",
     {
       resolve: "gatsby-plugin-react-svg",
       options: {
         rule: {
-          include: /assets/ // See below to configure properly
-        }
-      }
-    }
+          include: /assets/, // See below to configure properly
+        },
+      },
+    },
   ],
 };
