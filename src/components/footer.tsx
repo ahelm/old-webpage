45b4775ad4aa503c402604fd074df2224b23c57a
Anton Helm <anton.helm@tecnico.ulisboa.pt>
2021-01-24 18:47:28 +0000

    adds footer file

---
+++ src/components/footer.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import {
+  FaGithub, FaLinkedin, FaRegEnvelope, FaTwitter,
+} from 'react-icons/fa';
+
+const Footer = () => (
+  <footer>
+    <a href="https://twitter.com/HelmAnton" aria-label="Twitter">
+      <FaTwitter />
+    </a>
+    <a href="https://github.com/ahelm" aria-label="Github">
+      <FaGithub />
+    </a>
+    <a
+      href="https://www.linkedin.com/in/anton-helm/"
+      aria-label="Linkedin"
+    >
+      <FaLinkedin />
+    </a>
+    <a href="mailto:a.helm87@gmail.com" aria-label="Mail">
+      <FaRegEnvelope/>
+    </a>
+  </footer>
+);
+export default Footer;
