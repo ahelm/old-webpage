f1429191558e69f4566139ca649314cb6a4ed60d
Anton Helm <anton.helm@tecnico.ulisboa.pt>
2021-01-24 19:55:13 +0000

    updates IndexPage

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,28 +2,13 @@ import React from 'react';
 import Layout from '../components/layout';
 import SEO from '../components/seo';
 
-const AboutPage = () => (
+const IndexPage = () => (
   <Layout>
     <SEO title="About" />
     <div>
-      Hi, I&apos;m
-      {' '}
-      <span
-        style={{
-          backgroundColor: 'rgb(82, 131, 216)',
-          padding: '0.1rem 0.5rem',
-          color: 'white',
-        }}
-      >
-        Anton
-      </span>
+      Anton Helm
     </div>
-    <p>
-      I am a Ph.D. graduate in plasma physics at Instituto Superior Técnico.
-      Welcome to my webpage, and from time to time, I will try to post some tech
-      content here on my blog. Make sure to check it out.
-    </p>
-  </Layout>
+  </Layout >
 );
 
-export default AboutPage;
+export default IndexPage;
