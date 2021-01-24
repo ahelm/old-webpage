7dda5c3c4be54e59b0fea0729b8b9b18daaa729d
Anton Helm <anton.helm@tecnico.ulisboa.pt>
2021-01-24 18:48:23 +0000

    removes styles and adds footer to layout.tsx

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import Header from './header';
-import './layout.css';
+import Footer from './footer';
 
 interface Props {
   children: React.ReactNode
@@ -9,29 +9,8 @@ interface Props {
 const Layout = ({ children }: Props) => (
   <>
     <Header />
-    <div
-      style={{
-        margin: '0 auto',
-        maxWidth: 960,
-        padding: '0 1.0875rem 1.45rem',
-      }}
-    >
-      <main>{children}</main>
-      <footer
-        style={{
-          margin: '2.2rem 0 0',
-          textAlign: 'center',
-          fontSize: '0.8rem',
-        }}
-      >
-        Site created using
-        {' '}
-        <a href="https://www.gatsbyjs.com/">Gatsby</a>
-        {' '}
-        <br />
-        &copy; Anton Helm
-      </footer>
-    </div>
+    <main>{children}</main>
+    <Footer />
   </>
 );
 
