756d1d9b9c82f38a76585f428361a80a0b713c1f
Anton Helm <anton.helm@tecnico.ulisboa.pt>
2021-01-27 23:06:30 +0000

    adds projects page

---
+++ src/pages/projects.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Layout from '../components/layout';
+import SEO from '../components/seo';
+
+const ProjectPage = () => (
+  <Layout>
+    <SEO title="Projects" />
+    <div>
+      Projects
+    </div>
+  </Layout >
+);
+
+export default ProjectPage;
