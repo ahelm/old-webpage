d70e34916cf3cd87dc2f31b0f4d79d4c8807afa9
Anton Helm <anton.helm@tecnico.ulisboa.pt>
2021-01-27 23:22:12 +0000

    updates layout and wraps it it basic containers

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -8,9 +8,13 @@ interface Props {
 
 const Layout = ({ children }: Props) => (
   <>
-    <Header />
-    <main>{children}</main>
-    <Footer />
+    <div className="flex justify-around min-h-screen bg-gray-100">
+      <div className="bg-gray-50 flex-auto antialiased max-w-screen-lg shadow-xl">
+        <Header />
+        <main>{children}</main>
+        <Footer />
+      </div>
+    </div>
   </>
 );
 
