1e4b271f1d5544b4c8628a929cc8f45c45d254bc
Anton Helm <anton.helm@tecnico.ulisboa.pt>
2021-01-29 00:10:42 +0000

    updates footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -4,22 +4,27 @@ import {
 } from 'react-icons/fa';
 
 const Footer = () => (
-  <footer>
-    <a href="https://twitter.com/HelmAnton" aria-label="Twitter">
-      <FaTwitter />
-    </a>
-    <a href="https://github.com/ahelm" aria-label="Github">
-      <FaGithub />
-    </a>
-    <a
-      href="https://www.linkedin.com/in/anton-helm/"
-      aria-label="Linkedin"
-    >
-      <FaLinkedin />
-    </a>
-    <a href="mailto:a.helm87@gmail.com" aria-label="Mail">
-      <FaRegEnvelope />
-    </a>
+  <footer className="flex flex-col py-2 text-gray-400">
+    <div className="flex place-content-center text-sm text-center">
+      <div>
+        <div>Created with Gatsby and Tailwind CSS</div>
+        <div>© Anton Helm</div>
+      </div>
+    </div>
+    <div className="flex flex-row py-2 place-content-center">
+      <a href="https://twitter.com/HelmAnton" aria-label="Twitter" className="px-2">
+        <FaTwitter className="h-5 w-5" />
+      </a>
+      <a href="https://github.com/ahelm" aria-label="Github" className="px-2">
+        <FaGithub className="h-5 w-5" />
+      </a>
+      <a href="https://www.linkedin.com/in/anton-helm/" aria-label="Linkedin" className="px-2">
+        <FaLinkedin className="h-5 w-5" />
+      </a>
+      <a href="mailto:a.helm87@gmail.com" aria-label="Mail" className="px-2">
+        <FaRegEnvelope className="h-5 w-5" />
+      </a>
+    </div>
   </footer>
 );
 export default Footer;
